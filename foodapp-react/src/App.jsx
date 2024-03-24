f490556275e605cc30745a225f3497aa49fd144d
Rashidjuma198 <rashidjumaoti@gmail.com>
2024-03-24 22:40:34 +0300

    Additions of few templates to the projects

--- foodapp-react/src/App.jsx
+++ foodapp-react/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import Search from "./components/Search";
-
+import Foodlist from "./components/Foodlist";
+import Nav from "./components/Nav";
+import "./App.css";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/FoodDetails";
 function App() {
+  const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Search />
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <Foodlist foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
--- foodapp-react/src/components/Search.jsx
+++ foodapp-react/src/components/Search.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
-import { useEffect } from "react";
-export default function Search() {
-  const [query, setQuery] = useState("");
-  //Syntax of useffect hook
-  //useEffect(()=>{},[]);
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "5d2e8a4445844aa69a4bfbe8b8f3d1af";
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("Pizza");
+  //useEffect
   useEffect(() => {
-    function demo() {
-      console.log("Demo Funtion Executed");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
     }
-    demo();
+    fetchFood();
   }, [query]);
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+        className={styles.input}
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
